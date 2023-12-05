09e6a0aab0ac17aa4c29bc1d82c1835cef7e2004
balogunofafrica <balogunakanbi.k@gmail.com>
2023-12-05 12:59:55 +0000

    bal

--- s.js
+++ s.js
@@ -16,7 +16,24 @@ export const base64ToHex = (str) => {
 };
 
 export const rsaEncryption = (password) => {
-  const publicKeyString = `-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA34t3ax2ZykvBUBDlLYpuSMYPfjG6VIcd9diFZsP4N9anqCj/En3wsaeJcrZ7w+GlQKTOhDwh+i0D/JPT9oq5\nrLaW7Q8N7wzn1ISEecr6TWLUIwSapwp9rxrFb6/YAKSf7S/dUaYmGPmFFcroh3tT0nW0uDZSKxFiWy8De/99hlgAyxPqHB8znzgEnB+aROFzFVltbVuOcO/g7Q6mgWqV\nzZKlkE1+EROOMldqkS6VjIwXfosEv0P4bLTmBamL3YPkqk5TAErhc9auRdRyqwGbRv+ehQTnFspbE1vELqLkBq9nr+SQ5GdduukM3d5k/1Q+5PTq2qoZ6N9Gar/Z/rdd\nvQIDAQAB\n-----END PUBLIC KEY-----\n`;
+  const { publicKey } = Crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+    publicKeyEncoding: {
+      type: "spki",
+      format: "pem",
+    },
+    privateKeyEncoding: {
+      type: "pkcs8",
+      format: "pem",
+      cipher: "aes-256-cbc",
+      passphrase: "top secret",
+    },
+  });
+  console.log("🚀 ~ file: s.js:32 ~ rsaEncryption ~ publicKey:", publicKey);
+  console.log("\n\n...", JSON.stringify(publicKey, null, 2), "...\n\n");
+  const p0 = performance.now();
+  const publicKeyString = `-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA34t3ax2ZykvBUBDlLYpu\nSMYPfjG6VIcd9diFZsP4N9anqCj/En3wsaeJcrZ7w+GlQKTOhDwh+i0D/JPT9oq5\nrLaW7Q8N7wzn1ISEecr6TWLUIwSapwp9rxrFb6/YAKSf7S/dUaYmGPmFFcroh3tT\n0nW0uDZSKxFiWy8De/99hlgAyxPqHB8znzgEnB+aROFzFVltbVuOcO/g7Q6mgWqV\nzZKlkE1+EROOMldqkS6VjIwXfosEv0P4bLTmBamL3YPkqk5TAErhc9auRdRyqwGb\nRv+ehQTnFspbE1vELqLkBq9nr+SQ5GdduukM3d5k/1Q+5PTq2qoZ6N9Gar/Z/rdd\nvQIDAQAB\n-----END PUBLIC KEY-----\n`;
+  // const publicKeyString = `-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA5i1CyXzrrP2LAWeD8eIw\n5oE6/u/0fbEbAJatvbp8Zk+uRnmL5Nz4vvPoXzvDkdRNb0aQokHic0boYpU0WWyS\nR+PzWpZBgq5bzx36llAzDR6DWmBwB80u+gYzG9JEA3ROkO8ZWLt1XyzxO2143Jrw\nuVF/rqNitmSM1wVFnjgUhSvbmJoxqulGFdXleuhJH8yH7rpsLKJqfkWYuqdV/ght\n56SIqB3DPnj+lzryNqsWVUNyLS3Wt/TYULYSM+8Nzg21VRNyhPik+BEjuAz9en/q\nBqf2vEdg3AODxW5GHk8RW59wf6HnDYoBkm7XIgIGeRjW3QYukWqbP4QqfN8wwx1B\nCVAH6fHc7LFwNMelzjMsahb7x4dIcWMHQ9ku0oiJNUliJgGgQ3Hw+gIRWTqUS1/r\nI5P8Qfo5IMyiytF60KmmyaDz+elyUmxeQ101UU+tNZcoJlJqXqfRvAE44P4xP6/M\nBKZQJuFD9BYA8qvzJvNjD5hkgl0gGeEwI/sH/YbJ/wx0ArCugJiy+82Mah9BBlNy\nvKsfJDI5fXPDXqK/B3gx2ztRd2VXQKNqW+yxl3YM9nwB7mhDQ+NIzciFJZUOn7i2\nbiDRflNprjidVYajoA3p61xFWinZ2xWvlbEu7Vkb4R6xdVisMbm9YF0VL/IKwD7A\n0hcfDQszdOxkz4iSaxOFbZ0CAwEAAQ==\n-----END PUBLIC KEY-----\n`;
 
   let key = {
     key: publicKeyString,
@@ -26,6 +43,8 @@ export const rsaEncryption = (password) => {
     key,
     Crypto.createHash("sha256").update(password).digest()
   ).toString("base64");
+  const p1 = performance.now();
+  const diff = p1 - p0;
 
   return encryptedValue;
 };
